5de85082cc11835a53ff22928d136d43871349e2
Shawn Hagler <haglershawn@gmail.com>
2024-01-27 11:44:34 -0600

    added custom `Error` type

---
+++ src/error.rs
@@ -0,0 +1,28 @@
+
+/// Custom error type
+#[derive(Debug)]
+enum Error {
+    /// IO Error
+    IOError(std::io::Error),
+}
+
+/// Implement the formatter for our custom error type
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::IOError(err) =>
+                writeln!(f, "IO Error: {}", err);
+        }
+    }
+}
+
+/// Implement standard error trait and conversion from other error types
+impl std::error::Error for Error {}
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IOError(self)
+    }
+}
+
+/// Custom Result type alias
+type Result<T> = std::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+
+/// Programming languages
+enum Language {
+    C,
+}
+
+/// Programming language standards
+enum Standard {
+    C89,
+    C99,
+    C11,
+    C17
+}
+
+/// Build compilers
+enum Compiler {
+    /// GNU Compiler Collection (linux default)
+    GCC,
+
+    /// C/C++ LLVM Compiler
+    CLANG,
+
+    /// Microsoft Visual C++ (windows default)
+    MSVC,
+}
+
+/// Build type
+enum Type {
+    /// (default)
+    Executable,
+
+    ///
+    Library,
+
+    /// 
+    DynLibrary,
+}
+
+/// Build target
+enum Target {
+    X64_WINDOWS_MSVC,
+}
+
+enum Mode {
+    /// (default)
+    Debug,
+
+    /// Debug symbols removed and optimization enabled
+    Release,
+}
+
+struct Config {
+    language: Language,
+    standard: Standard,
+    compiler: Compiler,
+    build_type: Type,
+    target: Target,
+    mode: Mode,
+}
+
+struct Arguments {
+    command: String,
+    project_name: Option<String>,
+    config: Config,
+}
+
+fn parse_arguments() -> Result<Arguments> {
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
